328aa3120876e93518a06d4ecd7b640425e29344
GHKyrie <nemasterya@gmail.com>
2021-02-05 18:16:16 +0500

    Creation

---
+++ js/script.js
@@ -0,0 +1,72 @@
+const TESTER = document.getElementById('tester');
+const btn = document.getElementsByName('btn')[0];
+
+const f = (x, y) => Math.abs(x * y);
+
+const getXY = (a, b, n) => {
+    const delta = Math.abs(b - a) / n;
+    let data = [];
+
+    for (let i = a; i <= b; i += delta) 
+        data.push(i);
+
+    return data;
+};
+
+const getZ = (x, y) => {
+    let data = [];
+    let value;
+
+    for (let i = 0; i < x.length; i++) {
+        let temp = [];
+        for (let j = 0; j < y.length; j++) {
+            value = f(x[i], y[j]);
+            temp.push(value);
+        }
+        data.push(temp);
+    }
+    
+    return data;
+};
+
+btn.addEventListener('click', () => {
+    const a = Number(document.getElementsByTagName('input')[0].value);
+    const b = Number(document.getElementsByTagName('input')[1].value);
+    const n = Number(document.getElementsByTagName('input')[2].value);
+
+    const o_size_w = 850;
+    const o_size_h = 850;
+    const o_opacity = 0.75;
+    const o_autosize = false;
+    const o_title = 'z = f(x, y) = abs(x * y)';
+    const o_xtitle = 'X';
+    const o_ytitle = 'Y';
+    const o_ztitle = 'Z';
+    const o_type = 'surface';
+
+    const x0 = getXY(a, b, n);
+    const y0 = getXY(a, b, n);
+    const z0 = getZ(x0, y0);
+
+    const data_z = {
+        x: x0,
+        y: y0,
+        z: z0, 
+        type: o_type,
+        opacity: o_opacity
+    };
+
+    const layout = {
+        title: o_title,
+        scene: {
+            xaxis: {title: o_xtitle},
+            yaxis: {title: o_ytitle},
+            zaxis: {title: o_ztitle}
+        },
+        autosize: o_autosize,
+        width: o_size_w,
+        height: o_size_h,
+    };
+
+    Plotly.newPlot(TESTER, [data_z], layout);
+});
